537be698868e7772c177ee472937fa939f4ab889
chrischep <christine.chepleting@student.moringaschool.com>
2022-08-22 14:24:16 +0300

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(name, event){
+    let message =[];
+    for(let c=0;c<name.length; c++){
+    message.push(`Thank you, ${name[c]}, for the wonderful ${event} gift!`)
+    }
+    return message;
+}
+console.log(writeCards(["Guadalupe","Ollie","Aki"],"surprise"));
+function countDown(number){
+    while(number >=0){
+        console.log(number);
+        number--;
+    }
+}
